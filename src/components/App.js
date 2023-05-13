54214aa0974ab370e77d1091e75db47c28d822ef
CodewithShailly <121444727+Shailly2253@users.noreply.github.com>
2023-05-13 19:28:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,27 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from 'react';
 
-class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  handleClick() {
+    this.setState({ clicked: true });
+  }
+
+  render() {
+    return (
+      <div>
+        <button id="click" onClick={this.handleClick}>Click me</button>
+        {this.state.clicked && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+      </div>
+    );
+  }
 }
 
-
-export default App;
+export default Button;
 
